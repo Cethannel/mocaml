249bc0382edf1ce887de725b620aeb2bbfa45b7f
Ethan Nel <cethannel@protonmail.com>
2024-05-13 19:03:05 -0700

    Fixed the repl

--- lib/repl.ml
+++ lib/repl.ml
@@ -1,25 +1,28 @@
 let prompt = ">> "
 
-let rec start in_ out =
-  Out_channel.output_string out prompt;
-  Out_channel.flush out;
+let start in_ out =
   let env = Environment.init () in
-  match In_channel.input_line in_ with
-  | Some scanned ->
-    let lex = Lexer.new_lex scanned in
-    let parser = Parser.init lex in
-    let program = Parser.parse parser in
-    let _ =
-      match program with
-      | Error msg -> Fmt.pr "%a@." Parser.pp_parse_error msg
-      | Ok program ->
-        let str =
-          match Evaluator.eval program env with
-          | Error msg -> msg
-          | Ok out -> Object.show out
-        in
-        Out_channel.output_string out str
-    in
-    start in_ out
-  | None -> ()
+  let rec loop () =
+    Out_channel.output_string out prompt;
+    Out_channel.flush out;
+    match In_channel.input_line in_ with
+    | Some scanned ->
+      let lex = Lexer.new_lex scanned in
+      let parser = Parser.init lex in
+      let program = Parser.parse parser in
+      let _ =
+        match program with
+        | Error msg -> Fmt.pr "%a@." Parser.pp_parse_error msg
+        | Ok program ->
+          let str =
+            match Evaluator.eval program env with
+            | Error msg -> msg
+            | Ok out -> Object.show out
+          in
+          Out_channel.output_string out @@ str ^ "\n"
+      in
+      loop ()
+    | None -> ()
+  in
+  loop ()
 ;;
